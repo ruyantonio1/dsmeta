dcd61b4cd77e1dce5ea568c1de59fb5232059b74
Ruy Antonio <ruyantonio@gmail.com>
2022-07-12 21:07:05 -0300

    Fist component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import NotificationButton from "./components/notifictionButton"
 
 function App() {
   return(
-  <h1>Olá Mundo</h1>
+    <>
+      <h1>Olá Mundo</h1>
+      <NotificationButton/>
+    </>
   )
 }
 
